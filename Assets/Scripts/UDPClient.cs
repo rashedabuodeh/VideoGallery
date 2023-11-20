32fc16355d833fe84d723b3200f213723fa42bd2
Rashed Abuodeh <RASHED20101998@GMAIL.COM>
2023-11-20 17:39:23 +0300

    - Added UDP protocol

---
+++ Assets/Scripts/UDPClient.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+public class UDPClient : MonoBehaviour
+{
+    private UdpClient udpClient;
+    private IPEndPoint remoteEndPoint;
+
+    private void Start()
+    {
+        // Example: Start the UDP client and connect to the remote server
+        StartUDPClient("127.0.0.1", 5555);
+    }
+
+    private void StartUDPClient(string ipAddress, int port)
+    {
+        udpClient = new UdpClient();
+        remoteEndPoint = new IPEndPoint(IPAddress.Parse(ipAddress), port);
+
+        // Start receiving data asynchronously
+        udpClient.BeginReceive(ReceiveData, null);
+
+        // Send a message to the server
+        SendData("Hello, server!");
+    }
+
+    private void ReceiveData(IAsyncResult result)
+    {
+        byte[] receivedBytes = udpClient.EndReceive(result, ref remoteEndPoint);
+        string receivedMessage = System.Text.Encoding.UTF8.GetString(receivedBytes);
+
+        Debug.Log("Received from server: " + receivedMessage);
+
+        // Continue receiving data asynchronously
+        udpClient.BeginReceive(ReceiveData, null);
+    }
+
+    private void SendData(string message)
+    {
+        byte[] sendBytes = System.Text.Encoding.UTF8.GetBytes(message);
+
+        // Send the message to the server
+        udpClient.Send(sendBytes, sendBytes.Length, remoteEndPoint);
+
+        Debug.Log("Sent to server: " + message);
+    }
+}
---
+++ Assets/Scripts/UDPServer.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class UDPServer : MonoBehaviour
+{
+    private UdpClient udpServer;
+    private IPEndPoint remoteEndPoint;
+
+    private void Start()
+    {
+        // Example: Start the UDP server on port 5555
+        StartUDPServer(5555);
+    }
+
+    private void StartUDPServer(int port)
+    {
+        udpServer = new UdpClient(port);
+        remoteEndPoint = new IPEndPoint(IPAddress.Any, port);
+
+        Debug.Log("Server started. Waiting for messages...");
+
+        // Start receiving data asynchronously
+        udpServer.BeginReceive(ReceiveData, null);
+    }
+
+    private void ReceiveData(IAsyncResult result)
+    {
+        byte[] receivedBytes = udpServer.EndReceive(result, ref remoteEndPoint);
+        string receivedMessage = System.Text.Encoding.UTF8.GetString(receivedBytes);
+
+        Debug.Log("Received from client: " + receivedMessage);
+
+        // Process the received data
+
+        // Continue receiving data asynchronously
+        udpServer.BeginReceive(ReceiveData, null);
+    }
+
+    private void SendData(string message, IPEndPoint endPoint)
+    {
+        byte[] sendBytes = System.Text.Encoding.UTF8.GetBytes(message);
+
+        // Send the message to the client
+        udpServer.Send(sendBytes, sendBytes.Length, endPoint);
+
+        Debug.Log("Sent to client: " + message);
+    }
+}
